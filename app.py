f6ca28263e6f6059d27bcd9724b4dafc3e351350
manulangat <emmanuelthedeveloper@gmail.com>
2020-06-22 16:41:15 +0300

    ft-not_adding_up-backend

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, abort
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VideoGrant
+
+load_dotenv()
+twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+twilio_api_key_sid = os.environ.get('TWILIO_API_KEY_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
 
 app = Flask(__name__)
 
@@ -6,3 +15,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.get_json(force=True).get('username')
+    if not username:
+        abort(401)
+    print(username)
+    token = AccessToken(twilio_account_sid, twilio_api_key_sid,
+                        TWILIO_AUTH_TOKEN, identity=username)
+    token.add_grant(VideoGrant(room='My Room'))
+    # print(token)
+    return {'token': token.to_jwt().decode()}
